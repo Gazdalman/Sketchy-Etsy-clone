c74a27a44f0f5e2bbb8d40c2c4ff6a6aaa00a945
Roderick <Roderickj800@gmail.com>
2023-11-18 09:19:32 -0500

    product routes

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,3 +1,17 @@
 from flask import Blueprint
 from flask_login import login_required
 from app.models import Product, Review
+
+product_routes = Blueprint('products', __name__, url_prefix="/products")
+
+
+@product_routes.route('/')
+def products():
+    """Query to see all products"""
+
+
+@product_routes.route('/<int:id>')
+def specific_product():
+    """Query to see specific product"""
+    product = Product.query.get(id)
+    return product.to_dict()
