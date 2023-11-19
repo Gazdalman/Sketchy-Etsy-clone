1ff9d20a3b34d52d6f80ff50d9338cbc10aee3f4
Lillith13 <krystina.bell@outlook.com>
2023-11-19 14:54:27 -0500

    fixed shopping cart AGAIN -.-

--- app/api/routes/shoppingcart_routes.py
+++ app/api/routes/shoppingcart_routes.py
@@ -10,5 +10,6 @@ def shoppingCart():
     userId = current_user.get_id()
     cart = Cart.query.filter(Cart.user_id == userId).first()
     cartDict = cart.to_dict()
+    cartDict["cart"] = [product.to_dict() for product in cartDict["cart"]] # * toggle this line in and out of comment if it ever stops working -.- typically the line that fucks shit up lol
     print("Cart ==> ", cartDict["cart"])
     return cartDict["cart"]
