950f8aa02cf698d7234e70cc6e09ed9d9b30b6ed
AnnPrim <Primpraowann@gmail.com>
2023-11-20 17:27:45 -0600

    add to wishlist

--- react-app/src/components/Wishlist/index.js
+++ react-app/src/components/Wishlist/index.js
@@ -6,29 +6,32 @@ import OpenModalButton from "../OpenModalButton";
 import DeleteWish from "../DeleteModal/deleteModalWishlist";
 
 export default function Wishlist() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user);
-  const wishlist = useSelector((state) => state.wishlist);
-  const allProducts = wishlist.products
-    ? Object.values(wishlist.products)
-    : null;
-  // const [isLoaded, setIsLoaded] = useState(false);
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.session.user);
+    const wishlist = useSelector((state) => state.wishlist);
+    const allProducts = wishlist.products ? Object.values(wishlist.products) : null
+    const [isLoaded, setIsLoaded] = useState(false);
 
-  console.log("product id", wishlist.products);
-  console.log("all product", allProducts);
+    console.log('product id', wishlist.products)
+    console.log('all product', allProducts)
 
-  useEffect(() => {
-    if (!user) {
-      history.push("/login");
-    }
-    if (user) {
-      dispatch(getWish());
-      // .then(() => setIsLoaded(true))
-    }
-  }, [dispatch]);
+    useEffect(() =>  {
 
-  return (
+            if (user){
+                dispatch(getWish())
+                .then(() => setIsLoaded(true))
+            }
+
+    }, [dispatch]);
+
+
+  if (!user) {
+      history.push("/login")
+  };
+
+
+  return isLoaded && (
     <>
       <h1>Wishlist</h1>
       {allProducts && allProducts.length > 0 && (
