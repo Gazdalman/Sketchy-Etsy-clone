75c00853caef1e426fa6826ddd31591d749ff1c8
Roderick <Roderickj800@gmail.com>
2023-11-20 16:39:42 -0500

    trying to make the product reviews appear

--- app/api/routes/reviews_routes.py
+++ app/api/routes/reviews_routes.py
@@ -5,11 +5,20 @@ from app.forms import ReviewForm
 
 review_routes = Blueprint("reviews", __name__, url_prefix='/reviews')
 
-@review_routes.route("/<int:id>")
-# @login_required
-def get_all(id):
+@review_routes.route("/")
+def get_all_user_reviews(productId):
   """
-  Returns a list of all reviews
+  Returns a list of all product reviews
+  """
+  reviews = Review.query.filter(Review.product_id == productId).all()
+  review_list = {'reviews': [review.to_dict() for review in reviews]}
+  return review_list
+
+@review_routes.route("/<int:id>")
+@login_required
+def get_all_user_reviews(id):
+  """
+  Returns a list of all user reviews
   """
   reviews = Review.query.filter(Review.user_id == id).all()
   review_list = {'reviews': [review.to_dict() for review in reviews]}
