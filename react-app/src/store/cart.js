37f65c34138f7cb7b8fe5b2c739f3aeed22c4b17
Toney <ta.winston@outlook.com>
2023-11-21 08:17:17 -0600

    Merge with nina_dev-V@

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -56,19 +56,36 @@ export const updateQuantity = (itemId, change) => async (dispatch) => {
   }
 };
 
-export const addToCart = (itemId) => async (dispatch) => {
-  // ! need to create the backend route for adding an item to cart
-  console.log(itemId)
-  const res = await fetch("");
+export const removeItem = (itemId) => async (dispatch) => {
+  const res = await fetch(`/api/cart/product/${itemId}`, {
+    method: "DELETE",
+  });
   if (res.ok) {
     const cartData = await res.json();
-    if (cartData.errors) {
-      console.log(cartData.errors);
+    if (cartData.error) {
+      console.log(cartData.error);
       return;
     }
-    dispatch(addItemToCart(cartData));
+    // console.log(cartData);
+    dispatch(getCart());
   }
-}
+};
+
+export const updateQuantity = (itemId, change) => async (dispatch) => {
+  console.log(itemId);
+  const res = await fetch(`/api/cart/${change}/${itemId}`, {
+    method: "PUT",
+  });
+  if (res.ok) {
+    const cartData = await res.json();
+    if (cartData.error) {
+      console.log(cartData.error);
+      return;
+    }
+    // console.log(cartData);
+    dispatch(getCart());
+  }
+};
 
 export default function reducer(state = { cart: [] }, action) {
   // * CRUD -> create on signup & delete on checkout
@@ -78,6 +95,6 @@ export default function reducer(state = { cart: [] }, action) {
       new_state = { ...action.payload.cart };
       return new_state;
     default:
-      return state;
+      return state
   }
 }
