6d32d6bfae5bfe0c87c42a59ad45fb89f8a3b190
Toney <ta.winston@outlook.com>
2023-11-21 20:11:23 -0600

    production stuff

--- app/models/wishlist.py
+++ app/models/wishlist.py
@@ -36,6 +36,9 @@ class Wishlist (db.Model):
 class WishlistDetail(db.Model):
     __tablename__ = "wishlist_details"
 
+    if environment == "production":
+     __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
-    wishlist_id = db.Column(db.Integer, db.ForeignKey("wishlists.id"))
-    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    wishlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("wishlists.id")))
+    product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("products.id")))
