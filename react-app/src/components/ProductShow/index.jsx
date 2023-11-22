9b92f69d15ab466606fb762d33c7990ed9c3e52f
Roderick <Roderickj800@gmail.com>
2023-11-22 15:24:33 -0500

    review commenter implemented

--- react-app/src/components/ProductShow/index.jsx
+++ react-app/src/components/ProductShow/index.jsx
@@ -66,18 +66,15 @@ const ProductShow = () => {
         </span>
       </div>
       <h4 id="product-owner">Sold by {product?.seller}</h4>
+      <h4 id="product-owner">Sold by {product?.seller}</h4>
       <div id="product-details-lower">
         <p id="product-description">{product?.description}</p>
         {/* <CallOutBox numReviews={numReviews} avgRating={revAvg.toFixed(1)} product={product} /> */}
       </div>
-      {user && user.id != product.seller_id && (
-        <OpenModalButton
-          buttonText="Add to Cart"
-          modalComponent={<ConfirmAdd product={product} user={user} />}
-        />
-      )}
-      {user && user.id == product.seller_id && (
-        <button onClick={edit}>Edit Product</button>
+      {user?.id != product?.seller_id && (
+        <button value={product.id} onClick={(e) => handleClick(e, product)}>
+          Add to cart
+        </button>
       )}
       {/* <ReviewArea setRevAvg={setRevAvg} numRevs={setNumReviews} revAvg={revAvg} product={product} /> */}
       <Reviews product={product} />
