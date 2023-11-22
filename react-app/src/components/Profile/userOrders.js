92447f55fcc04747540c759221130161ebda6099
Lillith13 <krystina.bell@outlook.com>
2023-11-22 11:40:54 -0500

    fixed buttons and alert message

--- react-app/src/components/Profile/userOrders.js
+++ react-app/src/components/Profile/userOrders.js
@@ -7,10 +7,10 @@ import { addItemToCart, updateQuantity } from "../../store/cart";
 export default function UserOrders({ user }) {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.orders);
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart);
   const [isLoaded, setIsLoaded] = useState(false);
 
-  console.log('orders Arr', Object.values(orders))
+  console.log("orders Arr", Object.values(orders));
 
   useEffect(() => {
     dispatch(getAllOrders()).then(() => setIsLoaded(true));
@@ -18,11 +18,14 @@ export default function UserOrders({ user }) {
 
   const handleClick = (e) => {
     e.preventDefault();
-    e.target.innerText = "No...";
-    e.target.style.color = "red";
     if (e.target.innerText == "No...") {
       // * PopUp w/ message => "You don't even know how we got it to you, how do you think you're going to return it -.- "
+      const message =
+        "You don't even know how your packages are appearing in your house, how do you think you're going to return them if we did give you the option? 🤣";
+      alert(message);
     }
+    e.target.innerText = "No...";
+    e.target.style.color = "red";
   };
 
   const handleAddToCart = (e, product) => {
@@ -37,30 +40,34 @@ export default function UserOrders({ user }) {
 
   return isLoaded ? (
     <div>
-      {Object.values(orders).toReversed().slice(0, 1).map((order) => (
-        <div key={order.id}>
-          {order.products.map((item) => (
-            <div key={item.id}>
-              <h4>{item.name}</h4>
-              <p>{item.price}</p>
-              <button disabled>Write Review</button>
-              <button onClick={(e) => handleClick(e)}>Return Item</button>
-              <button value={item.id}
-                onClick={(e) => handleAddToCart(e, item)}>Buy Again</button>
-            </div>
-          ))}
-        </div>
-      ))}
+      {Object.values(orders)
+        .toReversed()
+        .slice(0, 1)
+        .map((order) => (
+          <div key={order.id}>
+            {order.products.map((item) => (
+              <div key={item.id}>
+                <h4>{item.name}</h4>
+                <p>{item.price}</p>
+                <button disabled>Write Review</button>
+                <button onClick={(e) => handleClick(e)}>Return Item</button>
+                <button
+                  value={item.id}
+                  onClick={(e) => handleAddToCart(e, item)}
+                >
+                  Buy Again
+                </button>
+              </div>
+            ))}
+          </div>
+        ))}
       <div style={{ marginTop: 30 }}>
         <div>
-          <Link
-            to="/past-order"
-            style={{ textDecoration: 'none' }}>
-
+          <Link to="/past-order" style={{ textDecoration: "none" }}>
             See All Orders...
           </Link>
         </div>
       </div>
     </div>
-  ) : null
+  ) : null;
 }
