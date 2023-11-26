0ff6f5879c5f714c8a28b309445e0f6700175c95
Toney <ta.winston@outlook.com>
2023-11-26 17:36:57 -0600

    Minor fixes

--- react-app/src/components/ProductPage/index.jsx
+++ react-app/src/components/ProductPage/index.jsx
@@ -94,9 +94,7 @@ const ProductPage = () => {
                     className="add-wish-btn"
                     onClick={(e) => addToWish(e, product)}
                   >
-                    {user &&
-                    userWish.products &&
-                    userWish.products[product.id] ? (
+                    {user && userWish.products && userWish.products[product.id] ? (
                       <i className="fa-solid fa-heart"></i>
                     ) : (
                       <i className="fa-regular fa-heart"></i>
