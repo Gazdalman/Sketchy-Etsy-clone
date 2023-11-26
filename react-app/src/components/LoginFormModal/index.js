2b340b89ea8d140e12ad641231286c23cd8729fc
Toney <ta.winston@outlook.com>
2023-11-26 17:36:57 -0600

    Minor fixes

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -7,10 +7,11 @@ import "./LoginForm.css";
 import OpenModalButton from "../OpenModalButton";
 import SignupFormModal from "../SignupFormModal";
 import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
-  const history = useHistory();
+  const history = useHistory()
   const [creds, setCreds] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -25,7 +26,7 @@ function LoginFormModal() {
       setErrors(data);
     } else {
       closeModal();
-      history.push("/home");
+      history.push("/home")
     }
   };
 
