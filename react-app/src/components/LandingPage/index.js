89c303371d15e5f0be1daca32f1d9edc324493db
AnnPrim <Primpraowann@gmail.com>
2023-11-26 08:47:08 -0600

    fixed wishlist button

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -6,19 +6,19 @@ import logo from "../../assets/Logo.png";
 import "./LandingPage.css";
 
 function LandingPage() {
-  return (
-    <>
-      <h1>Hello from Landing </h1>
-      <div className="landing-container">
-        <img src={logo} alt="logo" style={{ width: 600, height: 600 }} />
-        <Link to="/home">
-          <button id="landing-btn" onClick={{}}>
-            YOU’VE WON $500 CLICK NOW!
-          </button>
-        </Link>
-      </div>
-    </>
-  );
-}
+    return (
+        <div id="landing-page">
+            <div className="landing-container">
+                <img src={logo} alt="logo" style={{width:600, height:600}}/>
+                <Link to="/home" >
+                    <button
+                    id="landing-btn"
+                    onClick={{}}
+                    >YOU’VE WON $500 CLICK NOW!</button>
+                </Link>
+            </div>
+        </div>
 
-export default LandingPage;
+    );
+}
+export default LandingPage
