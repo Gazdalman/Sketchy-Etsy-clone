bbc33c402a44b31fad255107aa5c022121eaede3
AnnPrim <Primpraowann@gmail.com>
2023-11-26 22:39:03 -0600

    checkout page completed

--- react-app/src/components/CheckOut/ominousMessage.js
+++ react-app/src/components/CheckOut/ominousMessage.js
@@ -5,6 +5,8 @@ export default function CheckOutMessage() {
   const { closeModal } = useModal();
 
   return (
+    <>
+    {/* <img src={logo} alt="logo" style={{width:300, height:300}}/> */}
     <div className="checkout-message">
       <p style={{fontSize:30}} className="blue"> Just Kidding! 😈 </p>
       <p className="purple">We already know where you live...</p>
