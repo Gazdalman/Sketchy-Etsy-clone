fe8d3165da4fc1145eeb85481163f4c3d9b96072
Roderick <Roderickj800@gmail.com>
2023-11-27 15:46:44 -0500

    footer added

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,50 +1,56 @@
+import "./index.css";
 function Footer() {
   return (
-    <div className="bodody">
+    <>
       <h4>Take a Looker at our illustrious Creation Team</h4>
-      <h6>
-        The Naphthenic "Nina (Krystina) Bell"{" "}
-        <a href="https://github.com/Lillith13" target="#">
-          GitHub
-        </a>{" "}
-        <a
-          href="https://www.linkedin.com/in/krystina-bell-30523aab/"
-          target="#"
-        >
-          LinkedIn
-        </a>
-      </h6>
-      <h6>
-        The Assidious "Ann Mulling"{" "}
-        <a href="https://github.com/AnnMulling" target="#">
-          GitHub
-        </a>{" "}
-        <a href="https://www.linkedin.com/in/primpraow-m-653708227/" target="#">
-          LinkedIn
-        </a>
-      </h6>
-      <h6>
-        The Tachistoscopic "Toney Winston"{" "}
-        <a href="https://github.com/Gazdalman" target="#">
-          GitHub
-        </a>{" "}
-        <a
-          href="https://www.linkedin.com/in/toney-winston-53bb21141/"
-          target="#"
-        >
-          LinkedIn
-        </a>
-      </h6>
-      <h6>
-        The Rambunctious "Rod (Roderick) Johnson"{" "}
-        <a href="https://github.com/rrod11" target="#">
-          GitHub
-        </a>{" "}
-        <a href="https://www.linkedin.com/in/roderick-j-995394bb/" target="#">
-          LinkedIn
-        </a>
-      </h6>
-    </div>
+      <div className="bodody">
+        <h6 style={{ color: "red" }}>
+          The Naphthenic "Nina (Krystina) Bell"{" "}
+          <a href="https://github.com/Lillith13" target="#">
+            GitHub
+          </a>{" "}
+          <a
+            href="https://www.linkedin.com/in/krystina-bell-30523aab/"
+            target="#"
+          >
+            LinkedIn
+          </a>
+        </h6>
+        <h6 style={{ color: "orange" }}>
+          The Assidious "Ann Mulling"{" "}
+          <a href="https://github.com/AnnMulling" target="#">
+            GitHub
+          </a>{" "}
+          <a
+            href="https://www.linkedin.com/in/primpraow-m-653708227/"
+            target="#"
+          >
+            LinkedIn
+          </a>
+        </h6>
+        <h6 style={{ color: "green" }}>
+          The Tachistoscopic "Toney Winston"{" "}
+          <a href="https://github.com/Gazdalman" target="#">
+            GitHub
+          </a>{" "}
+          <a
+            href="https://www.linkedin.com/in/toney-winston-53bb21141/"
+            target="#"
+          >
+            LinkedIn
+          </a>
+        </h6>
+        <h6 style={{ color: "purple" }}>
+          The Rambunctious "Rod (Roderick) Johnson"{" "}
+          <a href="https://github.com/rrod11" target="#">
+            GitHub
+          </a>{" "}
+          <a href="https://www.linkedin.com/in/roderick-j-995394bb/" target="#">
+            LinkedIn
+          </a>
+        </h6>
+      </div>
+    </>
   );
 }
 
