676e1e18022931d02953cfa07711ce2bda430ff2
Toney Winston <137526632+Gazdalman@users.noreply.github.com>
2023-11-28 09:03:22 -0600

    Update product_routes.py

--- app/api/routes/product_routes.py
+++ app/api/routes/product_routes.py
@@ -21,7 +21,7 @@ def get_all():
   """
   Returns a list of all products on the site
   """
-  products = Product.query.filter(Product.seller_id > 0).all()
+  products = Product.query.filter(Product.seller_id > 0).order_by(Product.created_at.desc()).all()
   p_list = [product.to_dict() for product in products]
   return p_list
   # return render_template("test_products.html", p_list=p_list)
