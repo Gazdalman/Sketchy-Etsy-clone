22b5b1d0aae2c368d23085ed0847e1cf9a2ae171
Toney <ta.winston@outlook.com>
2023-11-27 19:26:14 -0600

    Fuck this project

--- react-app/src/components/ProductForm/index.jsx
+++ react-app/src/components/ProductForm/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createProduct, editProduct } from "../../store/product";
+import { createProduct, editProduct } from "../../store/asdfsadfasdfasdfas";
 import { useHistory, useParams } from "react-router-dom";
 import "./index.css";
 import "./ProductForm.css";
@@ -111,19 +111,7 @@ const ProductFormPage = ({ type, product }) => {
 
     setImageLoading(true);
     const id = await dispatch(createProduct(product, images));
-    if (id.errors) {
-      const errs = {};
-      for (const err in updated.errors) {
-        const parts = err.split(" : ");
-        errs[parts[0]] = parts[1];
-      }
-      setErrors(errs);
-      if (errors.not_found || errors.unauthorized) {
-        return history.replace("/");
-      }
-    } else {
-      return history.push(`/products/${id}`);
-    }
+    return history.push(`/products/${id}`);
   };
     const id = await dispatch(createProduct(product, images));
     return history.push(`/products/${id}`);
@@ -178,166 +166,159 @@ const ProductFormPage = ({ type, product }) => {
   };
 
   return !imageLoading ? (
-    <div className="mew-product-page">
+    <div className="productForm">
       <h1>What Are Yuh Sellin'?</h1>
-      <div className="new-product-form-container">
-        <form
-          onSubmit={type !== "edit" ? handleCreate : handleEdit}
-          encType="multipart/form-data"
-          className="new-product-form"
-        >
-          <span className="new-product-label">
-            Product Name*
-            {errors.name && <p>{errors.name}</p>}
-            <input
-              type="text"
-              placeholder="Name must be 3-50 characters"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </span>
-          <span className="new-product-label">
-            Product Category*
-            {errors.category && <p>{errors.category}</p>}
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value="">Select...</option>
-              <option value="Jackets and Coats">Jackets and Coats</option>
-              <option value="Shirts and Tops">Shirts and Tops</option>
-              <option value="Bottoms and Pants">Bottoms and Pants</option>
-              <option value="Kitchen/Home">Kitchen/Home</option>
-              <option value="Bathroom">Bathroom</option>
-              <option value="Gardening">Gardening</option>
-              <option value="Other">Other</option>
-              {/* Add more options as needed */}
-            </select>
-          </span>
-          <span className="new-product-label">
-            Description*
-            {errors.description && <p>{errors.description}</p>}
-            <textarea
-
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              maxLength={2000}
-              cols={50}
-              rows={5}
-              placeholder={
-                '10 or more characters \n(Add any tags at the bottom with an "#" before it)'
-              }
-            />
-            <p
-              className={`char-count ${
-                2000 - description.length <= 50 ? "low-count" : ""
-              }`}
-            >
-              Characters remaining: {2000 - description.length} / {2000}
-            </p>
-          </span>
-          {type != "edit" && (
-            <>
-              <span className="new-product-label">
-                Preview Image*
-                <input
-                  type="file"
-                  value={undefined}
-                  onChange={(e) => removeImgs(e)}
-                  accept="image/*"
-                />
-              </span>
-              <span className="new-product-label">
-                Add Image
-                <input
-                  type="file"
-                  disabled={!prevImg}
-                  value={undefined}
-                  onChange={(e) => setImg1(e.target.files[0])}
-                  accept="image/*"
-                />
-              </span>
-              <span className="new-product-label">
-                Add Image
-                <input
-                  type="file"
-                  value={undefined}
-                  disabled={!prevImg}
-                  onChange={(e) => setImg2(e.target.files[0])}
-                  accept="image/*"
-                />
-              </span>
-              <span className="new-product-label">
-                Add Image
-                <input
-                  type="file"
-                  value={undefined}
-                  disabled={!prevImg}
-                  onChange={(e) => setImg3(e.target.files[0])}
-                  accept="image/*"
-                />
-              </span>
-              <span className="new-product-label">
-                Add Image
-                <input
-                  type="file"
-                  disabled={!prevImg}
-                  value={undefined}
-                  onChange={(e) => setImg4(e.target.files[0])}
-                  accept="image/*"
-                />
-              </span>
-              <span className="new-product-label">
-                Add Image
-                <input
-                  type="file"
-                  value={undefined}
-                  disabled={!prevImg}
-                  onChange={(e) => setImg5(e.target.files[0])}
-                  accept="image/*"
-                />
-              </span>
-            </>
-          )}
-          <span className="new-product-label">
-            Price
-            {errors.price && <p>{errors.price}</p>}
-            <input
-              type="number"
-              step=".01"
-              value={checkPrice(price)}
-              placeholder="$USD"
-              onChange={e => setPrice(e.target.value)}
-              onBlur={(e) => {
-                const parsedValue = parseFloat(e.target.value).toFixed(2);
-                setPrice(parsedValue);
-              }}
-            />
-          </span>
-          <span className="new-product-label">
-            Units Available
-            {errors.units_available && <p>{errors.units_available}</p>}
-            <input
-              type="number"
-              min={type == 'edit' ? 0 : 1}
-              value={unitsAvailable}
-              placeholder="# of units you have"
-              onChange={(e) => setUnitsAvailable(e.target.value)}
-            />
-          </span>
-          <div className="new-product-btns">
-            <button type="submit" disabled={disabled}  className="submit-create">
-              {type == "edit" ? "Edit Product" : "Create Product"}
-            </button>
-            <button onClick={(e) => goBack(e)} className="cancel-create">Cancel</button>
-          </div>
-        </form>
-
-      </div>
+      <form
+        onSubmit={type !== "edit" ? handleCreate : handleEdit}
+        encType="multipart/form-data"
+      >
+        <span>
+          Product Name*
+          {errors.name && <p>{errors.name}</p>}
+          <input
+            type="text"
+            placeholder="Name must be 3-50 characters"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </span>
+        <span>
+          Product Category*
+          {errors.category && <p>{errors.category}</p>}
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">Select...</option>
+            <option value="Jackets and Coats">Jackets and Coats</option>
+            <option value="Shirts and Tops">Shirts and Tops</option>
+            <option value="Bottoms and Pants">Bottoms and Pants</option>
+            <option value="Kitchen/Home">Kitchen/Home</option>
+            <option value="Bathroom">Bathroom</option>
+            <option value="Gardening">Gardening</option>
+            <option value="Other">Other</option>
+            {/* Add more options as needed */}
+          </select>
+        </span>
+        <span>
+          Description*
+          {errors.description && <p>{errors.description}</p>}
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            maxLength={2000}
+            cols={50}
+            rows={5}
+            placeholder={
+              '10 or more characters \n(Add any tags at the bottom with an "#" before it)'
+            }
+          />
+          <p
+            className={`char-count ${
+              2000 - description.length <= 50 ? "low-count" : ""
+            }`}
+          >
+            Characters remaining: {2000 - description.length} / {2000}
+          </p>
+        </span>
+        {type != "edit" && (
+          <>
+            <span>
+              Preview Image*
+              <input
+                type="file"
+                value={undefined}
+                onChange={(e) => removeImgs(e)}
+                accept="image/*"
+              />
+            </span>
+            <span>
+              Add Image
+              <input
+                type="file"
+                disabled={!prevImg}
+                value={undefined}
+                onChange={(e) => setImg1(e.target.files[0])}
+                accept="image/*"
+              />
+            </span>
+            <span>
+              Add Image
+              <input
+                type="file"
+                value={undefined}
+                disabled={!prevImg}
+                onChange={(e) => setImg2(e.target.files[0])}
+                accept="image/*"
+              />
+            </span>
+            <span>
+              Add Image
+              <input
+                type="file"
+                value={undefined}
+                disabled={!prevImg}
+                onChange={(e) => setImg3(e.target.files[0])}
+                accept="image/*"
+              />
+            </span>
+            <span>
+              Add Image
+              <input
+                type="file"
+                disabled={!prevImg}
+                value={undefined}
+                onChange={(e) => setImg4(e.target.files[0])}
+                accept="image/*"
+              />
+            </span>
+            <span>
+              Add Image
+              <input
+                type="file"
+                value={undefined}
+                disabled={!prevImg}
+                onChange={(e) => setImg5(e.target.files[0])}
+                accept="image/*"
+              />
+            </span>
+          </>
+        )}
+        <span>
+          Price
+          {errors.price && <p>{errors.price}</p>}
+          <input
+            type="number"
+            step=".01"
+            value={checkPrice(price)}
+            placeholder="$USD"
+            onChange={(e) => setPrice(e.target.value)}
+            onBlur={(e) => {
+              const parsedValue = parseFloat(e.target.value).toFixed(2);
+              setPrice(parsedValue);
+            }}
+          />
+        </span>
+        <span>
+          Units Available
+          {errors.units_available && <p>{errors.units_available}</p>}
+          <input
+            type="number"
+            min={type == "edit" ? 0 : 1}
+            value={checkUnits(unitsAvailable)}
+            placeholder="# of units you have"
+            onChange={(e) => setUnitsAvailable(e.target.value)}
+          />
+        </span>
+        <button type="submit" disabled={disabled}>
+          {type == "edit" ? "Edit Product" : "Create Product"}
+        </button>
+        <button onClick={(e) => goBack(e)}>Cancel</button>
+      </form>
     </div>
   ) : (
     <h1>We loading...</h1>
   );
 };
 
-export default ProductFormPage;
+export default ProductFormPage
