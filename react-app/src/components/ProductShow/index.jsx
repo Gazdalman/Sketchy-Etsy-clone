8c1e3f6fb0680639c5ca44b2cf8233d769e5170e
Lillith13 <krystina.bell@outlook.com>
2023-12-11 17:55:07 -0500

    changed cart to be handled w/ localStorage

--- react-app/src/components/ProductShow/index.jsx
+++ react-app/src/components/ProductShow/index.jsx
@@ -40,6 +40,40 @@ const ProductShow = () => {
     return history.push(`/products/${productId}/edit`);
   };
 
+  const handleClick = (e, product) => {
+    e.preventDefault();
+    const message = "Item added to your shopping cart! 😊";
+    alert(message);
+    // const prodId = prod.id;
+    // if (cart[prodId]) {
+    //   dispatch(updateQuantity(prodId, "inc"));
+    // } else {
+    // dispatch(addItemToCart(prodId));
+    // }
+    let currCart = null;
+
+    currCart = localStorage.getItem(`${user.id}Cart`);
+
+    let updateCart = {};
+    if (currCart) {
+      const cart = JSON.parse(currCart);
+
+      if (cart[product.id]) {
+        cart[product.id].quantity++;
+        updateCart = { ...cart };
+      } else {
+        product.quantity = 1;
+        updateCart = { ...cart };
+        updateCart[product.id] = product;
+      }
+    } else {
+      product.quantity = 1;
+      updateCart[product.id] = product;
+    }
+
+    localStorage.setItem(`${user.id}Cart`, JSON.stringify(updateCart));
+  };
+
   return Object.keys(product).length > 0 &&
     +product.id === +productId &&
     +product.seller_id ? (
