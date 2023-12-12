1e17452c706722a07a60d2e319028d2eb7c4c8de
AnnPrim <Primpraowann@gmail.com>
2023-12-11 18:10:00 -0600

    first try

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -16,34 +16,35 @@ function Navigation({ isLoaded }) {
   const location = useLocation()
   return (
     <div className="nav-main-container">
-      <div className="nav-sub-container1">
 
-          <NavLink className="nav-link"  to="/home">
+      <div className="nav-sub-container-logo">
+          <NavLink className="nav-link" to="/home">
             <div
-            style={{fontSize:50, cursor: "pointer"}}
-            className="nav-btn">
+              style={{ fontSize: 50, cursor: "pointer" }}
+              className="nav-btn">
               <i className="fa-solid fa-skull "></i>
             </div>
           </NavLink>
 
-        <div className="category">
-          <div onClick={(e) => handleClick(e)}>Categories <i class="fa-solid fa-caret-down"></i></div>
-        </div>
 
+          <div className="category">
+            <div onClick={(e) => handleClick(e)}>Categories <i class="fa-solid fa-caret-down"></i></div>
+          </div>
       </div>
-        <div className="search-bar">
-          <div onClick={(e) => handleClick(e)}>Search <i class="fa-solid fa-magnifying-glass"></i></div>
-        </div>
 
-        <div className="nav-sub-container">
+      <div className="search-bar">
+        <div onClick={(e) => handleClick(e)}>Search <i class="fa-solid fa-magnifying-glass"></i></div>
+      </div>
 
-              <NavLink className="nav-link" to="/wishlist">
-                <div
-                  style={{fontSize: 33}}
-                  className="nav-btn">
-                  <i className="fa-solid fa-heart"></i>
-                </div>
-              </NavLink>
+      <div className="nav-sub-container">
+
+        <NavLink className="nav-link" to="/wishlist">
+          <div
+            style={{ fontSize: 33 }}
+            className="nav-btn">
+            <i className="fa-solid fa-heart"></i>
+          </div>
+        </NavLink>
 
 
               <NavLink className="nav-link" to="/cart">
