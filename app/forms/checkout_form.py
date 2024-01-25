a0e46e6930ec79c4ed1023c8e6c9e9ee5b534c20
AnnPrim <Primpraowann@gmail.com>
2024-01-24 21:54:01 -0600

    styled productreview and productpage

--- app/forms/checkout_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField
-from wtforms.validators import DataRequired
-
-class CheckoutForm(FlaskForm):
-    orderId = IntegerField('Order Id', [DataRequired()])
-    itemId = IntegerField('Product Id', [DataRequired()])
-    quantity = IntegerField('Product Quantity', [DataRequired()])
-    orderTotal = StringField('Order Total',  [DataRequired()])
