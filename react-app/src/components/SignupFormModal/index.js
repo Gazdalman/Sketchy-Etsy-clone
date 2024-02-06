7125172ae0f49fa7153b145229ff196e97dd5322
Toney Winston <137526632+Gazdalman@users.noreply.github.com>
2024-02-06 16:49:21 -0600

    Update index.js

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -70,7 +70,7 @@ function SignupFormModal() {
           Email
           <input
             className="signUpInput"
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
